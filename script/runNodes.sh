c98601c3ff52b880af4acfea2d813a48d9eb8200
dev-jb <bajracharyajenish@gmail.com>
2022-09-19 21:20:44 +0545

    add peer docker compose files

--- script/runNodes.sh
+++ script/runNodes.sh
@@ -4,3 +4,8 @@ Color_Off='\033[0m'
 
 ROOT_DIR=$PWD/../
 BTA_NETWORK_DIR=$ROOT_DIR/bta-network
+
+# docker-compose -f $BTA_NETWORK_DIR/ica/ica-o1-super-admin.yaml -f $BTA_NETWORK_DIR/ica/ica-o2-admin.yaml -f $BTA_NETWORK_DIR/ica/ica-o3-sh.yaml -f $BTA_NETWORK_DIR/ica/ica-o4-mlops.yaml -f $BTA_NETWORK_DIR/ica/ica-o5-ai-engineer.yaml up -d
+
+docker-compose -f $BTA_NETWORK_DIR/peers-c1/peer.o1-super-admin.yaml -f $BTA_NETWORK_DIR/peers-c1/peer.o2-admin.yaml -f $BTA_NETWORK_DIR/peers-c1/peer.o3-sh.yaml -f $BTA_NETWORK_DIR/peers-c1/peer.o4-mlops.yaml -f $BTA_NETWORK_DIR/peers-c1/peer.o5-ai-engineer.yaml up -d
+# docker-compose -f $BTA_NETWORK_DIR/peers-c1/peer.o1-super-admin.yaml -f $BTA_NETWORK_DIR/peers-c1/peer.o2-admin.yaml up -d
