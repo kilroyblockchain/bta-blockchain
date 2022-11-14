6944b27384bd49b5cf2d0797a5974605231c9ba6
Suraj <suraj@kilroyblockchain.com>
2022-11-14 16:24:33 +0545

    copy cloned bc connector to multiple as required

--- script/setupAndRunBlockchainConnector.sh
+++ script/setupAndRunBlockchainConnector.sh
@@ -4,7 +4,24 @@ mkdir ../bta-bc-connector
 
 cd ../bta-bc-connector
 
+sudo rm -r bc-connector
+
 git clone https://bitbucket.org/kilroy/bc-connector.git
 
+cd bc-connector && sudo rm -r .git 
+
+cd ..
+
+cp -r bc-connector bta-bc-connector-o1-super-admim
+
+cp -r bc-connector bta-bc-connector-o2-admin
+
+cp -r bc-connector bta-bc-connector-o3-sh
+
+cp -r bc-connector bta-bc-connector-o4-mlops
+
+cp -r bc-connector bta-bc-connector-o5-ai-engineer
+
+sudo rm -r bc-connector
 
 pwd
