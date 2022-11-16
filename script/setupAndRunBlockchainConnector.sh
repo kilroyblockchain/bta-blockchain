c6d696031b3e651b77ff51bad72f2be7f7b60208
Suraj <suraj@kilroyblockchain.com>
2022-11-17 00:20:28 +0545

    remove danling docker images

--- script/setupAndRunBlockchainConnector.sh
+++ script/setupAndRunBlockchainConnector.sh
@@ -4,6 +4,7 @@ LIGHT_CYAN='\033[0;96m'
 GRAY='\033[0;90m'
 BOLD_Green='\033[1;32m'
 BOLD_YELLOW="\033[1;33m"
+Red='\033[0;31m'
 Color_Off='\033[0m'
 
 export BC_CONNECTOR=bc-connector
@@ -102,93 +103,82 @@ cp -r ../../bta-ca/fabric-ca-client/org-ca/ica-$1-bta-kilroy $CRYPTO_FILES_DIR
 echo -e "${Magenta}Successfully setup crypto files of $1 for blockchain connector....${Color_Off}"
 }
 
+# Function for remove docker danling images
+removeDanlingImages(){
+echo -e "${Red}Starting removing docker danling images${Color_Off}"
+REMOVE_DANGLING_IMAGES="docker rmi $(docker images -q -f dangling=true)"
+eval $REMOVE_DANGLING_IMAGES
+echo -e "${Red}Successfully removed docker danling images${Color_Off}"
+}
 
 # Function run bc connector on docker  
 function runBcConnectorOnDocker(){
 echo -e "${BOLD_Green}Starting docker for bta_bc_connector_$1${Color_Off}"
 docker compose up -d prod
+
+# Remove development stage image or unused image of the docker
+removeDanlingImages
+
 echo -e "${BOLD_Green}Started docker for bta_bc_connector_$1 Successfully${Color_Off}"
 }
-
-# Remove non-named image
 
 echo "======================================================================================================================================================================================================>"
 # Goto bta-bc-connector-o1-super-admin directory and setup .env file and setup connection profile
 cd $APP_NAME-$BC_CONNECTOR-$SUPER_ADMIN 
-
 # Set .env file for o1-super-admin
 setupDotEnv $SUPER_ADMIN
-
 # Setup connection profile for o1-super-admin
 setupConnectionProfile $SUPER_ADMIN $SUPER_ADMIN_CONNECTION_PROFILE
-
 # Copy the crypto-config from bta-ca and paste on the blockchain connector at src/blockchain-files/crypto-files.
 setupCryptoFiles $SUPER_ADMIN
-
 # Up the docker for o1-super-admin
 runBcConnectorOnDocker $SUPER_ADMIN
 echo "======================================================================================================================================================================================================>"
 
 # Goto bta-bc-connector-o2-admin directory and setup .env file and setup connection profile
 cd ../$APP_NAME-$BC_CONNECTOR-$ADMIN 
-
 # Set .env file for o2-admin
 setupDotEnv $ADMIN
-
 # Setup connection profile for o2-admin
 setupConnectionProfile $ADMIN $ADMIN_CONNECTION_PROFILE
-
 # Copy the crypto-config from bta-ca and paste on the blockchain connector at src/blockchain-files/crypto-files.
 setupCryptoFiles $ADMIN
-
 # Up the docker for o2-admin
 runBcConnectorOnDocker $ADMIN
 echo "======================================================================================================================================================================================================>"
 
 # Goto bta-bc-connector-o3-sh directory and setup .env file and setup connection profile
 cd ../$APP_NAME-$BC_CONNECTOR-$STAKEHOLDER
-
 # Set .env file for o3-sh
 setupDotEnv $STAKEHOLDER
-
 # Setup connection profile for o3-sh
 setupConnectionProfile $STAKEHOLDER $STAKEHOLDER_COONECTION_PROFILE
-
 # Copy the crypto-config from bta-ca and paste on the blockchain connector at src/blockchain-files/crypto-files.
 setupCryptoFiles $STAKEHOLDER
-
 # Up the docker for o3-sh
 runBcConnectorOnDocker $STAKEHOLDER
 echo "======================================================================================================================================================================================================>"
 
 # Goto bta-bc-connector-o4-mlops directory and setup .env file and setup connection profile
 cd ../$APP_NAME-$BC_CONNECTOR-$MLOPS
-
 # Set .env file for o4-mlops
 setupDotEnv $MLOPS
-
 # Setup connection profile for o4-mlops
 setupConnectionProfile $MLOPS $MLOPS_CONNECTION_PROFILE
-
 # Copy the crypto-config from bta-ca and paste on the blockchain connector at src/blockchain-files/crypto-files.
 setupCryptoFiles $MLOPS
-
 # Up the docker for o4-mlops
 runBcConnectorOnDocker $MLOPS
 echo "======================================================================================================================================================================================================>"
 
 # Goto bta-bc-connector-o5-ai-engineer directory and setup .env file and setup connection profile
 cd ../$APP_NAME-$BC_CONNECTOR-$AI_ENGINEER
-
 # Set .env file for o5-ai-engineer
 setupDotEnv $AI_ENGINEER
-
 # Setup connection profile for o5-ai-engineer
 setupConnectionProfile $AI_ENGINEER $AI_ENGINEER_PROFILE
-
 # Copy the crypto-config from bta-ca and paste on the blockchain connector at src/blockchain-files/crypto-files.
 setupCryptoFiles $AI_ENGINEER
-
 # Up the docker for o5-ai-engineer
 runBcConnectorOnDocker $AI_ENGINEER
 echo "======================================================================================================================================================================================================>"
