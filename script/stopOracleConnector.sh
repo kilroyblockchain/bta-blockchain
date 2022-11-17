b143162908914e87e4a9b2e43f5a3b4fe9866181
Suraj <suraj@kilroyblockchain.com>
2022-11-17 23:41:01 +0545

    check if orcale connector directory is exits or not

--- script/stopOracleConnector.sh
+++ script/stopOracleConnector.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
+GREEN='\033[0;32m'
 BLOD_GREEN='\033[1;32m'
 BOLD_YELLOW="\033[1;33m"
+RED='\033[0;31m'
+BLOD_RED='\033[1;31m'
 Color_Off='\033[0m'
 
 # Export oracle connector repo name
 export ORACLE_CONNECTOR_REPO=oracle-connector
+
+
+#  Check the if the user clone the oracle-connector or not
+if [ ! -d "$ORACLE_CONNECTOR_REPO" ];
+then 
+echo -e "${RED}"
+echo "---------------------------------------------------"
+echo -e "---------------------------------------------------${Color_Off}"
+echo -e "${BLOD_RED}Sorry!!! There Is No Oracle Connector Directory${Color_Off}"
+echo -e "${RED}---------------------------------------------------"
+echo "---------------------------------------------------"
+echo -e "${Color_Off}"
+echo -e ""
+exit 0 
+fi
 
 # Go to oracle-connector folder
 cd ../$ORACLE_CONNECTOR_REPO
