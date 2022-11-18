c1edf0e995be125bee8519a35157aaa0295ce8df
Suraj <suraj@kilroyblockchain.com>
2022-11-18 15:31:41 +0545

    updated oracle images name dev to prod

--- script/stopOracleConnector.sh
+++ script/stopOracleConnector.sh
@@ -7,7 +7,7 @@ BLOD_RED='\033[1;31m'
 Color_Off='\033[0m'
 
 # Export oracle connector repo name
-export ORACLE_CONNECTOR_REPO=oracle-connector
+export ORACLE_CONNECTOR_REPO=../oracle-connector
 
 #  Check the if the user clone the oracle-connector or not
 if [ ! -d "$ORACLE_CONNECTOR_REPO" ];
@@ -24,7 +24,7 @@ exit 0
 fi
 
 # Go to oracle-connector folder
-cd ../$ORACLE_CONNECTOR_REPO
+cd $ORACLE_CONNECTOR_REPO
 
 # Down The Oracle Connector Docker Container And Remove It's Volumes
 echo -e "${GREEN}"
@@ -42,7 +42,7 @@ echo -e "${GREEN}"
 echo "---------------------------------------------------"
 echo -e "---------------------------------------------------${Color_Off}"
 echo -e "${BLOD_GREEN}Removing Docker Image Of The Oracle Connector${Color_Off}"
-docker image rm -f redis:latest obc-connector-dev:1.0.0 
+docker image rm -f redis:latest obc-connector-prod:1.0.0 
 echo -e "${GREEN}"
 echo "---------------------------------------------------"
 echo -e "---------------------------------------------------${Color_Off}"
@@ -50,7 +50,7 @@ echo -e "${BLOD_GREEN}Successfully Removed Image Of The Oracle Connector${Color_
 
 
 echo -e "${BLOD_GREEN}Removing Oracle Connector directory${Color_Off}"
-sudo rm -rf ../$ORACLE_CONNECTOR_REPO;
+sudo rm -rf $ORACLE_CONNECTOR_REPO;
 echo -e "${BLOD_GREEN}Successfully Removed Oracle Connector directory${Color_Off}"
 
 
