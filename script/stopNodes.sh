3a3136b7cf3f35404a2f91fe5a9364b43da9718e
Suraj <suraj@kilroyblockchain.com>
2022-11-24 15:31:23 +0545

    add .env file path on the stopNode.sh script file

--- script/stopNodes.sh
+++ script/stopNodes.sh
@@ -9,14 +9,14 @@ Color_Off='\033[0m'
 ROOT_DIR=$PWD/../
 BTA_NETWORK_DIR=$ROOT_DIR/bta-network
 
-docker-compose -f $BTA_NETWORK_DIR/ica/ica-o1-super-admin.yaml -f $BTA_NETWORK_DIR/ica/ica-o2-admin.yaml -f $BTA_NETWORK_DIR/ica/ica-o3-sh.yaml -f $BTA_NETWORK_DIR/ica/ica-o4-mlops.yaml -f $BTA_NETWORK_DIR/ica/ica-o5-ai-engineer.yaml down --volumes
+docker-compose --env-file $BTA_NETWORK_DIR/ica/.env -f $BTA_NETWORK_DIR/ica/ica-o1-super-admin.yaml -f $BTA_NETWORK_DIR/ica/ica-o2-admin.yaml -f $BTA_NETWORK_DIR/ica/ica-o3-sh.yaml -f $BTA_NETWORK_DIR/ica/ica-o4-mlops.yaml -f $BTA_NETWORK_DIR/ica/ica-o5-ai-engineer.yaml down --volumes
 
-docker-compose -f $BTA_NETWORK_DIR/peers-c1/peer.o1-super-admin.yaml -f $BTA_NETWORK_DIR/peers-c1/peer.o2-admin.yaml -f $BTA_NETWORK_DIR/peers-c1/peer.o3-sh.yaml -f $BTA_NETWORK_DIR/peers-c1/peer.o4-mlops.yaml -f $BTA_NETWORK_DIR/peers-c1/peer.o5-ai-engineer.yaml down --volumes
+docker-compose --env-file $BTA_NETWORK_DIR/peers-c1/.env -f $BTA_NETWORK_DIR/peers-c1/peer.o1-super-admin.yaml -f $BTA_NETWORK_DIR/peers-c1/peer.o2-admin.yaml -f $BTA_NETWORK_DIR/peers-c1/peer.o3-sh.yaml -f $BTA_NETWORK_DIR/peers-c1/peer.o4-mlops.yaml -f $BTA_NETWORK_DIR/peers-c1/peer.o5-ai-engineer.yaml down --volumes
 
 # ORDERER
-docker-compose -f $BTA_NETWORK_DIR/orderer/docker-compose.yaml down --volumes
+docker-compose --env-file $BTA_NETWORK_DIR/orderer/.env -f $BTA_NETWORK_DIR/orderer/docker-compose.yaml down --volumes
 
-docker-compose -f $BTA_NETWORK_DIR/cli-c1/docker-compose.yaml down --volumes
+docker-compose --env-file $BTA_NETWORK_DIR/cli-c1/.env -f $BTA_NETWORK_DIR/cli-c1/docker-compose.yaml down --volumes
 
 sleep 2
 
